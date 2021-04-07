96b2e89bc28cdd91bc922beb68d36ea7bd0f46fe
proyectoFinal43GRU <82124321+proyectoFinal43GRU@users.noreply.github.com>
2021-04-07 17:41:54 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDPr0tNqidivCjvBjdqbXGezU-L5Gb41u8",
+    authDomain: "proyectofinal43gru.firebaseapp.com",
+    projectId: "proyectofinal43gru",
+    storageBucket: "proyectofinal43gru.appspot.com",
+    messagingSenderId: "938076086315",
+    appId: "1:938076086315:web:0dfc20ba419e1e4041ca6e",
+    measurementId: "G-2C50SJ0SCV"
+    });
